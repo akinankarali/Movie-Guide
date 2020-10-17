c39adfd68d37580369156e6d6700c29f39e2f597
Akbeta <34417875+Akbeta@users.noreply.github.com>
2020-10-17 23:59:45 +0300

    mock data created

---
+++ lib/utils/mock_data.dart
@@ -0,0 +1,49 @@
+class MockData {
+  static const String APP_NAME = "Movie Guide";
+
+  static const List<String> MOVIE_NAMES = [
+    "Kimi no na wa",
+    "Inception",
+    "The Lord Of The Rings : The Return of the King",
+    "Forrest Gump",
+    "The Terminal",
+    "Star Wars : Episode V - The Empire Strikes Back",
+    "The Matrix",
+    "Spirited Away",
+    "Wall-e",
+    "Back to the Future",
+    "Braveheart",
+    "Captain Fantastic"
+  ];
+
+  static const List<String> MOVIE_YEARS = [
+    "2016",
+    "2010",
+    "2003",
+    "1994",
+    "2004",
+    "1980",
+    "1999",
+    "2001",
+    "2008",
+    "1985",
+    "1995",
+    "2016"
+  ];
+
+  static const List<String> MOVIE_DETAILS = [
+    "\tMovies Subjects\n\n"
+        "\tTwo strangers find themselves linked in a bizarre way. When a connection forms, will distance be the only thing to keep them apart?\n\n"
+        "\tA thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.\n\n"
+        "\tGandalf and Aragorn lead the World of Men against Sauron's army to draw his gaze from Frodo and Sam as they approach Mount Doom with the One Ring.\n\n"
+        "\tThe presidencies of Kennedy and Johnson, the events of Vietnam, Watergate and other historical events unfold through the perspective of an Alabama man with an IQ of 75, whose only desire is to be reunited with his childhood sweetheart.\n\n"
+        "\tAn Eastern European tourist unexpectedly finds himself stranded in JFK airport, and must take up temporary residence there.\n\n"
+        "\tAfter the Rebels are brutally overpowered by the Empire on the ice planet Hoth, Luke Skywalker begins Jedi training with Yoda, while his friends are pursued by Darth Vader and a bounty hunter named Boba Fett all over the galaxy.\n\n"
+        "\tWhen a beautiful stranger leads computer hacker Neo to a forbidding underworld, he discovers the shocking truth--the life he knows is the elaborate deception of an evil cyber-intelligence.\n\n"
+        "\tDuring her family's move to the suburbs, a sullen 10-year-old girl wanders into a world ruled by gods, witches, and spirits, and where humans are changed into beasts.\n\n"
+        "\tIn the distant future, a small waste-collecting robot inadvertently embarks on a space journey that will ultimately decide the fate of mankind.\n\n"
+        "\tMarty McFly, a 17-year-old high school student, is accidentally sent thirty years into the past in a time-traveling DeLorean invented by his close friend, the eccentric scientist Doc Brown.\n\n"
+        "\tScottish warrior William Wallace leads his countrymen in a rebellion to free his homeland from the tyranny of King Edward I of England.\n\n"
+        "\tIn the forests of the Pacific Northwest, a father devoted to raising his six kids with a rigorous physical and intellectual education is forced to leave his paradise and enter the world, challenging his idea of what it means to be a parent.\n\n"
+  ];
+}
